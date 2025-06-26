508d3b935320e0cc916028f7b6c8668b64188881
SkyComic-hash <skygamechunnal@gmail.com>
2025-06-26 18:47:47 +0300

    UserModel.cs

--- CabSystem/UserModel.cs
+++ CabSystem/UserModel.cs
@@ -1,1 +1,12 @@
-
+public class UserModel
+{
+    public int UserId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Phone { get; set; }
+    
+    // Новые поля
+    public decimal TotalSpent { get; set; }
+    public decimal DiscountRate { get; set; } // Скидка (0.1 для 10%)
+}
