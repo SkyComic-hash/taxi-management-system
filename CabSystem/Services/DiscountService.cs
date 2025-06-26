209576e2ce0d093925b7f462842e5c0d7557d468
SkyComic-hash <skygamechunnal@gmail.com>
2025-06-26 18:56:00 +0300

    DiscountService.cs

--- CabSystem/Services/DiscountService.cs
+++ CabSystem/Services/DiscountService.cs
@@ -1,1 +1,14 @@
-
+public class DiscountService
+{
+    public void UpdateCustomerDiscount(UserModel customer)
+    {
+        if (customer.TotalSpent > 10000)
+            customer.DiscountRate = 0.15m; // 15%
+        else if (customer.TotalSpent > 5000)
+            customer.DiscountRate = 0.10m; // 10%
+        else if (customer.TotalSpent > 2000)
+            customer.DiscountRate = 0.05m; // 5%
+        else
+            customer.DiscountRate = 0;
+    }
+}
