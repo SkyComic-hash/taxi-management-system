ccf0aed503b7999e339892343841e2ebec4504f8
SkyComic-hash <skygamechunnal@gmail.com>
2025-06-26 19:36:11 +0300

    /

--- CabSystem/Admin.cs
+++ CabSystem/Admin.cs
@@ -1,45 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+using System;
 
 namespace CabSystem
 {
     public class Admin : Person
     {
-        public string AdminID { get; set; }
+        public int AdminId { get; set; } // Изменено на int для согласованности
+        public string AdminUsername { get; set; }
+        public string AdminPassword { get; set; }
 
-        private string adminUsername;
-
-        private string AdminPassword;
-        //Public property to access private field indirectly
-        public string _adminUsername
+        public Admin(int adminId, string adminUsername, string adminPassword, 
+                    string name, string address, string gender) 
+                    : base(name, address, gender)
         {
-            get { return adminUsername; }
-            set
-            {
-                adminUsername = value;
-            }
+            AdminId = adminId;
+            AdminUsername = adminUsername;
+            AdminPassword = adminPassword;
         }
-
-        public string _adminPassword
-        {
-            get { return AdminPassword; }
-            set { AdminPassword = value; }
-        }
-
-        public Admin(string adminID, string adminusername, string adminpassword, string name, string address, string gender) : base(name, address, gender)
-        {
-            AdminID = adminID;
-            _adminUsername = adminusername;
-            _adminPassword = adminpassword;
-        }
+        
         public override string ToString()
         {
-            return $"{Name}\n{Address}\n {Gender}";
+            return $"{Name}\n{Address}\n{Gender}";
         }
     }
 }
