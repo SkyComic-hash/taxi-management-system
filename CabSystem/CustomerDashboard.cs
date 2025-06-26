020d2fb65c66e683822028f43e726fd8d71501ac
SkyComic-hash <skygamechunnal@gmail.com>
2025-06-26 19:43:33 +0300

    /

--- CabSystem/CustomerDashboard.cs
+++ CabSystem/CustomerDashboard.cs
@@ -8,84 +8,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+private readonly Customer _currentCustomer;
 
-namespace CabSystem
+public CustomerDashboard(Customer customer)
 {
-    public partial class CustomerDashboard : Form
-    {
-        private string LogInUser;
-        CabRental cabRental;
-        DatabaseHelper dbHelper;
-        private string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+    InitializeComponent();
+    _currentCustomer = customer;
+    
+    // Обновляем интерфейс с учетом нового объекта
+    lblWelcome.Text = $"Welcome, {customer.Name}!";
+    lblTotalSpent.Text = $"Total spent: {customer.TotalSpent:C}";
+    lblDiscountRate.Text = $"Discount: {customer.DiscountRate:P0}";
+    
+    // Загрузка истории заказов
+    LoadOrderHistory();
+}
 
-        public CustomerDashboard(string username)
-        {
-            InitializeComponent();
-            this.LogInUser = username;
-            cabRental = new CabRental();
-            dbHelper = new DatabaseHelper(connectionString);
-        }
-
-        private void LogOutBtn_Click(object sender, EventArgs e)
-        {
-            Login login = new Login();
-            this.Close();
-            login.Show();
-        }
-
-        private void CloseBtn_Click(object sender, EventArgs e)
-        {
-            ButtonClicks.CloseButton_Click(sender, e, this);
-        }
-
-        private void MinimizeBtn_Click(object sender, EventArgs e)
-        {
-            ButtonClicks.MinimizeBtn_Click(sender, e, this);
-        }
-
-        private void CustomerDashboard_Load(object sender, EventArgs e)
-        {
-            string customerName = dbHelper.GetCustomerNameByUsername(LogInUser);
-            if (customerName != null)
-            {
-                nameBox.Text = customerName;
-            }
-
-            dataGridDrivers.DataSource = cabRental.LoadDriversTotable();
-            dataGridDrivers.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            foreach(DataGridViewColumn column in dataGridDrivers.Columns)
-            {
-                column.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
-            }
-
-
-            dataGridVehicles.DataSource = cabRental.LoadVehiclesToTable();
-            dataGridVehicles.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            foreach(DataGridViewColumn col in dataGridVehicles.Columns)
-            {
-                col.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
-            }
-        }
-
-        private void Personal_Details_Click(object sender, EventArgs e)
-        {
-            PersonalDetails personalDetails = new PersonalDetails(LogInUser);
-            this.Hide();
-            personalDetails.Show();
-        }
-
-        private void ordersbutton_Click(object sender, EventArgs e)
-        {
-            PersonalOrders personalOrders = new PersonalOrders(LogInUser);
-            this.Hide();
-            personalOrders.Show();
-        }
-
-        private void Rentbutton_Click(object sender, EventArgs e)
-        {
-            Booking booking = new Booking(LogInUser);
-            this.Hide();
-            booking.Show();
-        }
-    }
+private void LoadOrderHistory()
+{
+    // Логика загрузки истории заказов для текущего клиента
+    var orders = new OrderService().GetCustomerOrders(_currentCustomer.CustomerId);
+    dataGridViewOrders.DataSource = orders;
 }
